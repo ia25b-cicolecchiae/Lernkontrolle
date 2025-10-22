9e6aba480d63aedd021cf511e6042dfe8723e748
eneac <cicolecchiae@bzz.ch>
2025-10-22 09:23:20 +0200

    uhbjh

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        boolean schalter = true;
+
+        while (schalter) {
+            System.out.println("1: " + "Aufgabe hinzufügen" );
+            System.out.println("2: " + "Alle Aufgaben anzeigen");
+            System.out.println("3: " + "Aufgabe bearbeiten");
+            System.out.println("4: " + "Aufgabe löschen");
+            System.out.println("5: " + "Programm beenden");
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
